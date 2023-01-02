db3d09918401a2bb179405a89873c7cd7f0fa2a2
AL5399 <AL5399@ALIPL2672>
2023-01-02 15:22:18 +0530

    Add project files.

---
+++ First/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        ///sum 
+
+        /*int[] arr = new int[] { 1, 2, 3 };
+        int sum = arr.Sum(); ;
+
+        Console.WriteLine(sum);*/
+
+
+        ///if else
+        /*Console.WriteLine("Enter An Number");
+        int RollNumber = int.Parse(Console.ReadLine());
+        if (RollNumber == 25)
+        {
+            Console.WriteLine("Ravids Roll Number Is 25");
+        }
+
+        else  
+        {
+            Console.WriteLine("Its not his Roll Number");
+        }*/
+
+
+        ///Switch
+        /*Console.WriteLine("Enter An Number");
+        int RollNumber = int.Parse(Console.ReadLine());
+        switch (RollNumber)
+        {
+            case 25:
+                Console.WriteLine("its correct Number");
+                break;
+            case 15:
+                Console.WriteLine("its Not a correct Number");
+                break;
+            default:
+                Console.WriteLine("its Not a correct Number");
+                break;
+        }*/
+
+
+
+
+        ///tried string and array
+        
+
+        /*string name = "RAVID";
+        var replacement = name.Replace("RAVID", "RIO");
+        Console.WriteLine($"The source string is {name}");
+        Console.WriteLine($"The updated string is {replacement}");
+
+        Console.WriteLine(name);
+        
+
+        int[] myNum = { 10, 20, 30, 40 };
+        myNum[2] = 55;
+        for (int i = 0; i < myNum.Length; i++)
+        {
+            Console.WriteLine(myNum[i]);
+        }*/
+
+    }
+
+    }
+
+
