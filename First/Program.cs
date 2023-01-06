bddf18f1325efa885f581c391b745df2388b78bb
RavidAccion <ravid.rv@accionlabs.com>
2023-01-06 13:07:51 +0530

    Class

--- First/Program.cs
+++ First/Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using First;
+using System;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
 
-    class Program
+    /*class Program
     {
         static void Main()
-        {
+        {*/
         ///sum 
 
         /*int[] arr = new int[] { 1, 2, 3 };
@@ -46,7 +50,7 @@
 
 
         ///tried string and array
-        
+
 
         /*string name = "RAVID";
         var replacement = name.Replace("RAVID", "RIO");
@@ -63,8 +67,104 @@
             Console.WriteLine(myNum[i]);
         }*/
 
-    }
 
-    }
+        ///using StringBuilder
 
+        /*StringBuilder sb = new StringBuilder("Hello ");
+
+        sb.Append("World!!");
+        sb.Append("Hello C#!");
+        sb.Append("This is new line.");
+
+        Console.WriteLine(sb);
+        string s = "Hello All";
+        string sa=s.Substring(0, 2);
+        Console.WriteLine(sa);*/
+
+
+
+        ///Try Catch
+        /* try
+         {
+             Console.WriteLine("Enter An Number");
+             int Firstnum = int.Parse(Console.ReadLine());
+             Console.WriteLine("Enter Another Number To Divide");
+             int Secondnum = int.Parse(Console.ReadLine());
+             int Result = Firstnum / Secondnum;
+             Console.WriteLine(Result);
+         }
+ catch(Exception ex)
+         {
+             Console.WriteLine(ex.Message);
+         }
+         finally {
+             Console.WriteLine("This Is The Result");
+             Console.ReadLine(); }*/
+
+
+
+        ///Assign
+
+       /* int[] arr = { 10, 11, 12 };
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+
+           
+            for (int j = i; j < arr.Length; j += 2)
+            {
+                
+                for (int k = i; k <= j; k++)
+                {
+                    *//*Console.WriteLine(k);*//*
+
+                    sum += arr[k];
+                }
+            }
+        }
+        Console.WriteLine(sum);*/
+
+
+        /*int[,] accounts = new[,] { { 1, 2, 3 }, { 3, 2, 1 } };
+        int n = accounts.GetLength(1);
+        int wealth = 0;
+        int m = accounts.GetLength(0);
+        int MaxWealth = 0;
+        for (int i = 0; i < n; i++)
+        {
+
+            for (int j = 0; j < m; j++)
+            {
+
+                wealth += accounts[i][j];
+            }
+            MaxWealth = Math.Max(MaxWealth, wealth);
+        }
+
+        Console.WriteLine(MaxWealth);*/
+ /*   }
+ 
+
+}*/
+
+///OOPS
+ class program
+{
+    string Company = "Skoda";
+    string Name = "Kushaq";
+    string color = "White";
+    string Model = "2022";
+    string Condition = "Good";
+
+    static void Main(string[] args)
+    {
+        firstclass items = new firstclass();
+
+        items.One();
+        items.Two(3, 27);
+            }
+
+}
 
---
+++ First/firstclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    public class firstclass
+    {
+        int sum = 0;
+        public void One()
+        {
+            Console.WriteLine("This is my first class");
+        }
+        public void Two(int a ,int b)
+        {
+            sum=a+b;
+            Console.WriteLine("The sum of the number is =" + sum);
+        }
+    }
+}
